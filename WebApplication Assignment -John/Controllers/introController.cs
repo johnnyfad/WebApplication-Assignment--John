790efad7985ed6fad7d9ee8cab128e3466ab5483
johnnyfad <johnlejudge@gmail.com>
2023-03-16 14:38:05 -0400

    Add project files.

---
+++ WebApplication Assignment -John/Controllers/introController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_Assignment__John.Controllers
+{
+    public class introController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
